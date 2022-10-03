1abcaa7b1fb7e58209ed784be6862f152ee003c5
GabrielZCode <32156154@mackenzista.com.br>
2022-10-03 10:04:22 -0300

    MailSender Start

---
+++ WebFlux/Mail/src/test/kotlin/com/portifolio/Mail/MailSenderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.portifolio.Mail
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MailSenderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
