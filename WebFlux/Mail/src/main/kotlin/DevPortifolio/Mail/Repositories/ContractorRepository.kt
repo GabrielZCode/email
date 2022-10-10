66595fd4ed548aa2fe3c3d94cef8faf8f0362625
GabrielZCode <32156154@mackenzista.com.br>
2022-10-10 14:55:19 -0300

    Repositório JPA para Contratantes

--- WebFlux/Mail/src/main/kotlin/DevPortifolio/Mail/Repositories/ContractorRepository.kt
+++ WebFlux/Mail/src/main/kotlin/DevPortifolio/Mail/Repositories/ContractorRepository.kt
@@ -4,8 +4,8 @@ package DevPortifolio.Mail.Repositories
 import org.springframework.data.jpa.repository.JpaRepository
 import DevPortifolio.Mail.Entities.Contractor
 import org.springframework.stereotype.Repository
+import javax.persistence.Query
 
 @Repository
-interface ContractorRepository :  JpaRepository<Contractor, Long>{
-    
+interface ContractorRepository :  JpaRepository<Contractor, Long>{ 
   }
